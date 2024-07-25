84be7e548a38b3d3451f9538b66ac0d61aef76d4
KumaranMaddy <thirukumaran33008@gmail.com>
2024-07-25 20:48:07 +0530

    push

--- src/components/SegmentCreation/SegmentCreation.jsx
+++ src/components/SegmentCreation/SegmentCreation.jsx
@@ -10,15 +10,12 @@ const SegmentCreation = (props) => {
   return (
     <div className='about-segment-creation-section'>
         <div className='left-section'>
-            <h3>Build Your Own Data Segments with Ease</h3>
-            <p >Use our intuitive segment builder to select and customize data schema options, and create segments that fit your specific needs. Save time and gain insights by building custom data segments that reflect the unique characteristics of your audience.</p>
-            <div className='save-btn-container' onClick={handlePopup}> 
+           
+        <div className='save-btn-container' onClick={handlePopup}> 
                 <button className='save-segment-btn'>Save segment</button>
             </div>
         </div>
-        <div className='right-section'>
-            <img src = {segment} alt="segment" />
-        </div>
+        
     </div>
   )
 }
